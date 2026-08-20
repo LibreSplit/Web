cdfc668f0a33124bd3ef4dd1e065678c65cd1311
Luna <starlit.luna.eve@gmail.com>
2026-08-20 12:35:36 -0400

    Replace prettier with oxfmt. Add TS linting with oxlint.

---
+++ oxlint.config.ts
@@ -0,0 +1,29 @@
+import solid from "eslint-plugin-solid";
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+  categories: {
+    correctness: "error",
+    suspicious: "error",
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  ignorePatterns: ["dist/**"],
+  jsPlugins: ["eslint-plugin-solid"],
+  options: {
+    reportUnusedDisableDirectives: "error",
+  },
+  plugins: ["eslint", "typescript", "unicorn", "oxc", "import"],
+  rules: {
+    ...solid.configs.typescript.rules,
+    "import/no-unassigned-import": [
+      "error",
+      {
+        allow: ["**/*.css", "prismjs/components/*"],
+      },
+    ],
+    "typescript/consistent-type-definitions": ["error", "interface"],
+  },
+});
