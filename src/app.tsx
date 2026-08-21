8886afdeb708eb493f36f1be40af206a66d4237a
Luna <starlit.luna.eve@gmail.com>
2026-08-21 09:23:24 -0400

    Adjust size.

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ export default function App(props: ParentProps) {
   return (
     <div class="flex min-h-dvh flex-col">
       <AppNav />
-      <main class="mx-auto flex w-full max-w-5xl min-w-0 flex-1 flex-col px-4 py-6 sm:px-6 md:min-h-0 md:py-8 lg:px-8">
+      <main class="mx-auto flex w-full max-w-7xl min-w-0 flex-1 flex-col px-4 py-6 sm:px-6 md:min-h-0 md:py-8 lg:px-8">
         {props.children}
       </main>
     </div>
